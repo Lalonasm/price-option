b2a0f77b15efb5c2a4593ea9e93f24ad6a243d82
Lalon Sheikh <you@example.com>
2024-05-15 16:38:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import DaisyNav from "./componants/DaisyNav/DaisyNav"
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-6xl p-2 bg-rose-500'>Price-options</h1>
+
+
+    </>
+  )
+}
+
+export default App
